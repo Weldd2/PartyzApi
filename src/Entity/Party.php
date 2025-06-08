87e6732e81c3c2c17bb8521a6305d4261896a38d
Your Name <a.mura27400@gmail.com>
2025-06-08 11:14:21 +0200

    feat: enhance Party entity with user-specific operations and relationships

--- src/Entity/Party.php
+++ src/Entity/Party.php
@@ -2,184 +2,246 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
-use App\Repository\PartyRepository;
-use Symfony\Component\Serializer\Annotation\Groups;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Metadata\Link;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PartyRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Parameter;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 #[ORM\Entity(repositoryClass: PartyRepository::class)]
 #[ApiResource(
-    normalizationContext: ['groups' => ['party:read']],
+	normalizationContext: ['groups' => ['party:read']]
+)]
+#[ApiResource(
+	uriTemplate: '/users/{id}/parties',
+	uriVariables: [
+		'id' => new Link(
+			fromClass: User::class,
+			fromProperty: 'parties'
+		)
+	],
+	normalizationContext: ['groups' => ['user:party:read']],
+	operations: [new GetCollection()],
+	openapi: new Operation(
+		tags: ['User'],
+		summary: 'Get parties for a specific user',
+		description: 'Retrieves the Parties where the User is a member',
+		parameters: [
+			new Parameter(
+				name: 'id',
+				in: 'path',
+				description: 'User identifier',
+				required: true,
+				schema: ['type' => 'integer']
+			)
+		]
+	)
+)]
+#[ApiResource(
+	uriTemplate: '/users/{id}/partiesAsOwner',
+	uriVariables: [
+		'id' => new Link(
+			fromClass: User::class,
+			fromProperty: 'partiesAsOwner'
+		)
+	],
+	normalizationContext: ['groups' => ['user:party:read']],
+	operations: [new GetCollection()],
+	openapi: new Operation(
+		tags: ['User'],
+		summary: 'Get parties for a specific user',
+		description: 'Retrieves the collection of Party resources associated with a User.'
+	)
 )]
 class Party
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    #[Groups(["party:read"])]
-    private ?int $id = null;
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	#[Groups(['user:party:read', 'party:read'])]
+	private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(["party:read"])]
-    private ?string $title = null;
+	#[ORM\Column(length: 255)]
+	#[Groups(['user:party:read', 'party:read'])]
+	private ?string $title = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(["party:read"])]
-    private ?string $address = null;
+	#[ORM\Column(length: 255)]
+	#[Groups(['user:party:read', 'party:read'])]
+	private ?string $address = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(["party:read"])]
-    private ?string $postalCode = null;
+	#[ORM\Column(length: 255)]
+	private ?string $postalCode = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(["party:read"])]
-    private ?string $city = null;
+	#[ORM\Column(length: 255)]
+	private ?string $city = null;
 
-    #[ORM\Column]
-    #[Groups(["party:read"])]
-    private ?\DateTime $date = null;
+	#[ORM\Column]
+	#[Groups(['user:party:read', 'party:read'])]
+	private ?\DateTime $date = null;
 
-    /**
-     * @var Collection<int, User>
-     */
-    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'parties')]
-    #[Groups(["party:read"])]
-    private Collection $members;
+	/**
+	 * @var Collection<int, User>
+	 */
+	#[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'parties')]
+	#[Groups(['party:read'])]
+	#[MaxDepth(1)]
+	private Collection $members;
 
-    /**
-     * @var Collection<int, ShoppingListItem>
-     */
-    #[ORM\OneToMany(targetEntity: ShoppingListItem::class, mappedBy: 'party', orphanRemoval: true)]
-    #[Groups(["party:read"])]
-    private Collection $shoppingList;
+	/**
+	 * @var Collection<int, ShoppingListItem>
+	 */
+	#[ORM\OneToMany(targetEntity: ShoppingListItem::class, mappedBy: 'party', orphanRemoval: true)]
+	#[Groups(['party:read', 'party:shoppingListItem:read'])]
+	private Collection $shoppingList;
 
-    public function __construct()
-    {
-        $this->members = new ArrayCollection();
-        $this->shoppingList = new ArrayCollection();
-    }
+	#[ORM\ManyToOne(inversedBy: 'partiesAsOwner')]
+	#[ORM\JoinColumn(nullable: false)]
+	#[Groups(['user:party:read', 'party:read'])]
+	private ?User $owner = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function __construct()
+	{
+		$this->members = new ArrayCollection();
+		$this->shoppingList = new ArrayCollection();
+	}
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
+	public function getTitle(): ?string
+	{
+		return $this->title;
+	}
 
-        return $this;
-    }
+	public function setTitle(string $title): static
+	{
+		$this->title = $title;
 
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
+		return $this;
+	}
 
-    public function setAddress(string $address): static
-    {
-        $this->address = $address;
+	public function getAddress(): ?string
+	{
+		return $this->address;
+	}
 
-        return $this;
-    }
+	public function setAddress(string $address): static
+	{
+		$this->address = $address;
 
-    public function getPostalCode(): ?string
-    {
-        return $this->postalCode;
-    }
+		return $this;
+	}
 
-    public function setPostalCode(string $postalCode): static
-    {
-        $this->postalCode = $postalCode;
+	public function getPostalCode(): ?string
+	{
+		return $this->postalCode;
+	}
 
-        return $this;
-    }
+	public function setPostalCode(string $postalCode): static
+	{
+		$this->postalCode = $postalCode;
 
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
+		return $this;
+	}
 
-    public function setCity(string $city): static
-    {
-        $this->city = $city;
+	public function getCity(): ?string
+	{
+		return $this->city;
+	}
 
-        return $this;
-    }
+	public function setCity(string $city): static
+	{
+		$this->city = $city;
 
-    public function getDate(): ?\DateTime
-    {
-        return $this->date;
-    }
+		return $this;
+	}
 
-    public function setDate(\DateTime $date): static
-    {
-        $this->date = $date;
+	public function getDate(): ?\DateTime
+	{
+		return $this->date;
+	}
 
-        return $this;
-    }
+	public function setDate(\DateTime $date): static
+	{
+		$this->date = $date;
 
-    /**
-     * @return Collection<int, User>
-     */
-    public function getMembers(): Collection
-    {
-        return $this->members;
-    }
+		return $this;
+	}
 
-    public function addMember(User $member): static
-    {
-        if (!$this->members->contains($member)) {
-            $this->members->add($member);
-            $member->addParty($this);
-        }
+	/**
+	 * @return Collection<int, User>
+	 */
+	public function getMembers(): Collection
+	{
+		return $this->members;
+	}
 
-        return $this;
-    }
+	public function addMember(User $member): static
+	{
+		if (!$this->members->contains($member)) {
+			$this->members->add($member);
+			$member->addParty($this);
+		}
 
-    public function removeMember(User $member): static
-    {
-        if ($this->members->removeElement($member)) {
-            $member->removeParty($this);
-        }
+		return $this;
+	}
 
-        return $this;
-    }
+	public function removeMember(User $member): static
+	{
+		if ($this->members->removeElement($member)) {
+			$member->removeParty($this);
+		}
 
-    /**
-     * @return Collection<int, ShoppingListItem>
-     */
-    public function getShoppingList(): Collection
-    {
-        return $this->shoppingList;
-    }
+		return $this;
+	}
 
-    public function addShoppingList(ShoppingListItem $shoppingList): static
-    {
-        if (!$this->shoppingList->contains($shoppingList)) {
-            $this->shoppingList->add($shoppingList);
-            $shoppingList->setParty($this);
-        }
+	/**
+	 * @return Collection<int, ShoppingListItem>
+	 */
+	public function getShoppingList(): Collection
+	{
+		return $this->shoppingList;
+	}
 
-        return $this;
-    }
+	public function addShoppingList(ShoppingListItem $shoppingList): static
+	{
+		if (!$this->shoppingList->contains($shoppingList)) {
+			$this->shoppingList->add($shoppingList);
+			$shoppingList->setParty($this);
+		}
 
-    public function removeShoppingList(ShoppingListItem $shoppingList): static
-    {
-        if ($this->shoppingList->removeElement($shoppingList)) {
-            // set the owning side to null (unless already changed)
-            if ($shoppingList->getParty() === $this) {
-                $shoppingList->setParty(null);
-            }
-        }
+		return $this;
+	}
 
-        return $this;
-    }
+	public function removeShoppingList(ShoppingListItem $shoppingList): static
+	{
+		if ($this->shoppingList->removeElement($shoppingList)) {
+			// set the owning side to null (unless already changed)
+			if ($shoppingList->getParty() === $this) {
+				$shoppingList->setParty(null);
+			}
+		}
+
+		return $this;
+	}
+
+	public function getOwner(): ?User
+	{
+		return $this->owner;
+	}
+
+	public function setOwner(?User $owner): static
+	{
+		$this->owner = $owner;
+
+		return $this;
+	}
 }
